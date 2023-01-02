56b9ae453a360e1229177d51aa83df710080601d
Nati-Zerai <Natizerai11@gmail.com>
2023-01-03 03:48:59 +0400

    3rd commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import states_sample from "./states.json";
 import "./App.css";
 import axios from "axios";
 
+// reusualble checkbox
 const Checkbox = ({ label, value, onChange }) => {
   return (
     <div>
@@ -17,11 +18,12 @@ const Checkbox = ({ label, value, onChange }) => {
 };
 
 function App() {
+  //const for the inotial map location
   const center = [37.62514389160612, -99.67658554492868];
 
-  // Search
+  // const for the value to search
   const [search, setSearch] = useState("");
-  console.log("@@@@ Search is : " + search);
+
   // Add to Search
   const addToSearch = (value) => {
     setSearch((prevSearch) => {
@@ -31,6 +33,7 @@ function App() {
       return `${prevSearch}, ${value}`;
     });
   };
+
   // Remove from Search
   const removeFromSearch = (value) => {
     setSearch((prevSearch) => {
@@ -60,36 +63,36 @@ function App() {
     });
   };
 
-  // Get API
+  // my customed made API
+  const myAPI = "http://localhost:8080/confidence?group_category=" + search;
 
-  const myAPI =
-    "https://natnael-nodejs.herokuapp.com/confidence?geography_category=" +
-    search;
-  console.log("myAPI:::: " + myAPI + "  --  " + typeof myAPI);
+  //const to hold the fetched json
   const [post, setPost] = useState(states_sample);
 
   // function fetch data using axios
   function axiosFetch() {
     axios.get(myAPI).then((response) => {
       setPost(response.data);
-      console.log("hiii: ");
-      console.log("post_inside: " + JSON.stringify(response.data));
     });
   }
 
+  // Fetch on search change
   useEffect(() => {
-    // axiosFetch();
-  }, []);
-  console.log("byee: ");
-  console.log("post_outside: " + post);
+    if (search === "") {
+      setPost(states_sample);
+      return;
+    }
+    axiosFetch();
+  }, [search]);
 
-  //// Age
+  // Age checkbox states
   const [checkedAge_1, setCheckedAge_1] = useState(false);
   const [checkedAge_2, setCheckedAge_2] = useState(false);
   const [checkedAge_3, setCheckedAge_3] = useState(false);
   const [checkedAge_4, setCheckedAge_4] = useState(false);
   const [checkedAge_5, setCheckedAge_5] = useState(false);
-  //// Race / Ethnicity
+
+  // Race / Ethnicity checkbox states
   const [checkedRace_1, setCheckedRace_1] = useState(false);
   const [checkedRace_2, setCheckedRace_2] = useState(false);
   const [checkedRace_3, setCheckedRace_3] = useState(false);
@@ -97,28 +100,34 @@ function App() {
   const [checkedRace_5, setCheckedRace_5] = useState(false);
   const [checkedRace_6, setCheckedRace_6] = useState(false);
 
-  //// Handle Checkbox
+  // Handle Checkbox
   const valueMap = {
-    "5 - 11 years": [checkedAge_1, setCheckedAge_1],
-    "12 - 17 years": [checkedAge_2, setCheckedAge_2],
-    "18 - 49 years": [checkedAge_3, setCheckedAge_3],
-    "50 - 64 years": [checkedAge_4, setCheckedAge_4],
+    "5 – 11 years": [checkedAge_1, setCheckedAge_1],
+    "12 – 17 years": [checkedAge_2, setCheckedAge_2],
+    "18 – 49 years": [checkedAge_3, setCheckedAge_3],
+    "50 – 64 years": [checkedAge_4, setCheckedAge_4],
     "65+ years": [checkedAge_5, setCheckedAge_5],
+    "American Indian or Alaska Native": [checkedRace_1, setCheckedRace_1],
+    Asian: [checkedRace_2, setCheckedRace_2],
+    Black: [checkedRace_3, setCheckedRace_3],
+    "non-Hispanic": [checkedRace_4, setCheckedRace_4],
+    "Native Hawaiian or Other Pacific Islander": [
+      checkedRace_5,
+      setCheckedRace_5,
+    ],
+    White: [checkedRace_6, setCheckedRace_6],
   };
 
   const handleCheckboxChange = (event, value) => {
     const [state, setState] = valueMap[value];
     setState(event.target.checked);
-    console.log("XXXX event.target", event.target.checked);
-    if (event.target.checked == true) {
+    if (event.target.checked === true) {
       addToSearch(value);
-      axiosFetch();
     } else {
       removeFromSearch(value);
-      axiosFetch();
     }
   };
-  console.log("&&&&&&&&&checkedAge_1  " + checkedAge_1);
+
   return (
     <div>
       <div className="text-center m-8">
@@ -137,31 +146,31 @@ function App() {
             >
               <p className="font-bold"> Age </p>
               <Checkbox
-                label="5 - 11 years"
+                label="5 – 11 years"
                 value={checkedAge_1}
                 onChange={(event) =>
-                  handleCheckboxChange(event, "5 - 11 years")
+                  handleCheckboxChange(event, "5 – 11 years")
                 }
               />
               <Checkbox
-                label="12 - 17 years"
+                label="12 – 17 years"
                 value={checkedAge_2}
                 onChange={(event) =>
-                  handleCheckboxChange(event, "12 - 17 years")
+                  handleCheckboxChange(event, "12 – 17 years")
                 }
               />
               <Checkbox
-                label="18 - 49 years"
+                label="18 – 49 years"
                 value={checkedAge_3}
                 onChange={(event) =>
-                  handleCheckboxChange(event, "18 - 49 years")
+                  handleCheckboxChange(event, "18 – 49 years")
                 }
               />
               <Checkbox
-                label="50 - 64 years"
+                label="50 – 64 years"
                 value={checkedAge_4}
                 onChange={(event) =>
-                  handleCheckboxChange(event, "50 - 64 years")
+                  handleCheckboxChange(event, "50 – 64 years")
                 }
               />
               <Checkbox
@@ -172,9 +181,52 @@ function App() {
             </div>
             <div className="p-5 mb-1 border rounded-md bg-slate-100">
               <p className="font-bold"> Race/Ethnicity </p>
+              <Checkbox
+                label="American Indian or Alaska Native"
+                value={checkedRace_1}
+                onChange={(event) =>
+                  handleCheckboxChange(
+                    event,
+                    "American Indian or Alaska Native"
+                  )
+                }
+              />
+              <Checkbox
+                label="Asian"
+                value={checkedRace_2}
+                onChange={(event) => handleCheckboxChange(event, "Asian")}
+              />
+              <Checkbox
+                label="Black or African American"
+                value={checkedRace_3}
+                onChange={(event) => handleCheckboxChange(event, "Black")}
+              />
+              <Checkbox
+                label="Hispanic or Latino"
+                value={checkedRace_4}
+                onChange={(event) =>
+                  handleCheckboxChange(event, "non-Hispanic")
+                }
+              />
+              <Checkbox
+                label="Native Hawaiian or Other Pacific Islander"
+                value={checkedRace_5}
+                onChange={(event) =>
+                  handleCheckboxChange(
+                    event,
+                    "Native Hawaiian or Other Pacific Islander"
+                  )
+                }
+              />
+              <Checkbox
+                label="White"
+                value={checkedRace_6}
+                onChange={(event) => handleCheckboxChange(event, "White")}
+              />
             </div>
           </div>
-          <div className="px-5 mx-5">
+          <div className="mx-5">
+            {/* Map */}
             <MapContainer
               center={center}
               zoom={4}
@@ -185,73 +237,97 @@ function App() {
                 attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
               />
 
-              {console.log("-------post: " + post)}
+              {/* loop through the output of the fetch */}
               {post?.data.map((state) => {
+                // Store coordinates
+                const key = state.id;
                 const coordinates = state.geometry.coordinates[0].map(
                   (item) => [item[1], item[0]]
                 );
-                const myEstimate = state.properties.avg_estimate;
-                {
-                  /* console.log("avg_estimate " + myEstimate + "\n"); */
-                }
+                // Store estimate %
+                const estimate = state.properties.avg_estimate;
 
-                {
-                  /* 
-                  Converter num-to-color Algorithm 1
-                  console.log(
-                  "calc " +
-                    "#" +
-                    Math.round(myEstimate).toString(16).padStart(6, "0")
-                ); */
-                }
-
-                //// Converter num-to-color Algorithm 2
+                // Convert num-to-color
                 function percentageToHslColor(percentage) {
-                  const color = `hsl(${30 * (percentage / 100)}, 100%, 50%)`;
+                  const color = `hsl(${2 * percentage}, 100%, 45%)`;
                   return color;
                 }
 
                 return (
-                  <>
-                    <Polygon
-                      pathOptions={{
-                        fillColor: percentageToHslColor(myEstimate),
-                        fillOpacity: 0.7,
-                        weight: 2,
-                        opacity: 1,
-                        dashArray: 3,
-                        color: "white",
-                      }}
-                      positions={coordinates}
-                      eventHandlers={{
-                        mouseover: (e) => {
-                          const layer = e.target;
-                          layer.setStyle({
-                            dashArray: "",
-                            fillColor: "#00000",
-                            fillOpacity: 0.7,
-                            weight: 2,
-                            opacity: 1,
-                            color: "white",
-                          });
-                        },
-                        mouseout: (e) => {
-                          const layer = e.target;
-                          layer.setStyle({
-                            fillOpacity: 0.7,
-                            weight: 2,
-                            dashArray: "3",
-                            color: "white",
-                            fillColor: percentageToHslColor(myEstimate),
-                          });
-                        },
-                        click: (e) => {},
-                      }}
-                    />
-                  </>
+                  <Polygon
+                    key={key}
+                    pathOptions={{
+                      fillColor: percentageToHslColor(estimate),
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      dashArray: 3,
+                      color: "white",
+                    }}
+                    positions={coordinates}
+                    eventHandlers={{
+                      mouseover: (e) => {
+                        const layer = e.target;
+                        layer.setStyle({
+                          dashArray: "",
+                          fillColor: "#00000",
+                          fillOpacity: 0.7,
+                          weight: 2,
+                          opacity: 1,
+                          color: "white",
+                        });
+                      },
+                      mouseout: (e) => {
+                        const layer = e.target;
+                        layer.setStyle({
+                          fillOpacity: 0.7,
+                          weight: 2,
+                          dashArray: "3",
+                          color: "white",
+                          fillColor: percentageToHslColor(estimate),
+                        });
+                      },
+                      click: (e) => {},
+                    }}
+                  />
                 );
               })}
             </MapContainer>
+
+            {/* Legend of the Map */}
+            <div className="justify-center flex">
+              <span className="mr-1 border text-sm bg-[#FF0000] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 0</h2>
+
+              <span className="mr-1 border text-sm bg-[#FF5E00] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 10</h2>
+
+              <span className="mr-1 border text-sm bg-[#FFBB00] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 20</h2>
+
+              <span className="mr-1 border text-sm bg-[#E6FF00] w-10 h-2"></span>
+              <span className="mr-1 text-sm"> 30</span>
+
+              <span className="mr-1 border text-sm bg-[#88FF00] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 40</h2>
+              <span className="mr-1 border text-sm bg-[#2AFF00] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 50</h2>
+
+              <span className="mr-1 border text-sm bg-[#00FF33] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 60</h2>
+
+              <span className="mr-1 border text-sm bg-[#00FF90] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 70</h2>
+
+              <span className="mr-1 border text-sm bg-[#00FFEE] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 80</h2>
+
+              <span className="mr-1 border text-sm bg-[#00B2FF] w-10 h-2"></span>
+              <span className="mr-1 text-sm"> 90</span>
+
+              <span className="mr-1 border text-sm bg-[#004CFF] w-10 h-2"></span>
+              <h2 className="mr-1 text-sm"> 100</h2>
+            </div>
           </div>
         </div>
       </div>
